9ace7e512ec8e1867c8b658071e281da4d3f4089
Simon VDB <simonvdbroeck@gmail.com>
2018-05-01 22:58:06 +0200

    Handle polyfills correctly

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import styled from 'styled-components'
+import styled, { injectGlobal } from 'styled-components'
 
 import runPolyfills from '../util/polyfills'
-runPolyfills()
-
-import 'semantic-ui-css/semantic.min.css'
 
 import Header from '../components/header'
-
 import background from './trianglify2.svg'
+
+import 'normalize.css'
+
+injectGlobal`
+  html {
+    font-family: 'Roboto', sans-serif;
+  }
+`
 
 const Body = styled.div`
   background-image: url(${background});
@@ -18,27 +22,33 @@ const Body = styled.div`
   background-size: cover;
 `
 
-const Content = styled.div`
-  margin: 0 auto;
-  max-width: 960px;
-`
+class Layout extends React.Component {
+  static propTypes = {
+    children: PropTypes.func,
+  }
 
-const Layout = ({ children, data }) => (
-  <Body>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <Content>{children()}</Content>
-  </Body>
-)
+  componentDidMount() {
+    runPolyfills();
+    console.log('running polyfills');
+  }
 
-Layout.propTypes = {
-  children: PropTypes.func,
+  render() {
+    const { children, data } = this.props
+
+    return (
+      <Body>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        {children()}
+      </Body>
+    )
+  }
 }
 
 export default Layout
