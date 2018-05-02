dedc3ab180d1ec7e12f1cb6547686eb74b47d1d3
Simon VDB <simonvdbroeck@gmail.com>
2018-05-02 16:39:01 +0200

    Spread props over svg element in ArrowDown

--- src/svg/ArrowDown.js
+++ src/svg/ArrowDown.js
@@ -7,6 +7,7 @@ const ArrowDownSVG = props => (
     width="24"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
     <path d="M0-.75h24v24H0z" fill="none" />
