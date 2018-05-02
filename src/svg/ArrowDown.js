2d03cb76f7d61137cdf31bc777af661036078957
Simon VDB <simonvdbroeck@gmail.com>
2018-05-02 08:04:18 +0200

    Extract out SVG files into react components

---
+++ src/svg/ArrowDown.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+const ArrowDownSVG = props => (
+  <svg
+    fill="#000000"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
+    <path d="M0-.75h24v24H0z" fill="none" />
+  </svg>
+)
+
+export default ArrowDownSVG
