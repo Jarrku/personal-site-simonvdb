ffc272f232b0c529c5cbcc977d30e8ed50e38c70
Simon VDB <simonvdbroeck@gmail.com>
2018-05-18 00:42:56 +0200

    Self host font files

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,16 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
-import styled, { injectGlobal } from 'styled-components'
+import styled from 'styled-components'
 
 import runPolyfills from '../util/polyfills'
+import 'normalize.css'
+import './fonts.css';
 
 import Header from '../components/header'
 
-const background = require("./trianglify2.svg") as string;
+const background = require('./trianglify2.svg') as string
 // import background from './trianglify2.svg'
 
-import 'normalize.css'
-
-injectGlobal`
-  html {
-    font-family: 'Roboto', sans-serif;
-  }
-`
 
 const Body = styled.div`
   background-image: url(${background});
@@ -24,7 +19,6 @@ const Body = styled.div`
 `
 
 class Layout extends React.Component {
-
   componentDidMount() {
     runPolyfills()
   }
@@ -37,8 +31,15 @@ class Layout extends React.Component {
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Portfolio website for Simon Van den Broeck, Junior Frontend Engineer' },
-            { name: 'keywords', content: 'javascript, web development, react, frontend' },
+            {
+              name: 'description',
+              content:
+                'Portfolio website for Simon Van den Broeck, Junior Frontend Engineer',
+            },
+            {
+              name: 'keywords',
+              content: 'javascript, web development, react, frontend',
+            },
           ]}
         />
         <Header siteTitle={data.site.siteMetadata.title} />
