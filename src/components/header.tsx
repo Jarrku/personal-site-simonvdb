c2957092103b027972b3ebdfa5d6b02a2b36da3a
Simon VDB <simonvdbroeck@gmail.com>
2018-05-18 00:44:55 +0200

    Create header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,64 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
-
 import styled from 'styled-components'
+
+import { HomeSVG, AssignmentSVG, AccountCircleSVG } from '../svg'
 
 const StyledHeader = styled.div`
   z-index: 5;
   background-color: rgba(255, 255, 255, 0.7);
-  height: 10vh;
-  display: flex;
-  align-items: center;
+  height: 8vh;
   position: fixed;
   width: 100%;
+  display: flex;
 `
 
 const Container = styled.div`
+  flex: 1;
   margin: 0 auto;
   max-width: 960px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
-const Title = styled(Link)`
+const Title = styled.button`
   color: rgb(122, 160, 188);
   text-decoration: none;
 `
 
-const Header = ({ siteTitle }) => (
-  <StyledHeader>
-    <Container>
-      <h1 style={{ margin: 0 }}>
-        <Title to="/">{'Header Icons' || siteTitle}</Title>
-      </h1>
-    </Container>
-  </StyledHeader>
-)
+const Icon = styled.div`
+  padding: 10px;
+  flex: 0 1 0%;
+  :hover {
+    background-color: grey;
+  }
+`
+const goTo = (id: string) => {
+  window.document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+}
 
-export default Header
+const goToWelcome = () => window.scroll({ top: 0, left: 0, behavior: 'smooth' })
+const goToSkills = () => goTo('#skills')
+const goToIntro = () => goTo('#intro-container')
+
+
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <StyledHeader>
+        <Container>
+          <Icon onClick={goToWelcome}>
+            <HomeSVG width={36} height={36} />
+          </Icon>
+          <Icon onClick={goToIntro}>
+            <AccountCircleSVG width={36} height={36} />
+          </Icon>
+          <Icon onClick={goToSkills}>
+            <AssignmentSVG width={36} height={36} />
+          </Icon>
+        </Container>
+      </StyledHeader>
+    )
+  }
+}
