35f544c335536ddaa6ac89bc453de74d66b15aae
Simon VDB <simonvdbroeck@gmail.com>
2018-10-08 09:42:51 +0200

    remove GA script

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,6 @@ module.exports = {
     title: `Simon Van den Broeck`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-      },
-    },
     `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-manifest`,
